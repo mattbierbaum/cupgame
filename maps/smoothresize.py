4ab4c1487bc7fef00c318b0c871dcaad9af0ea94
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-01-14 13:59:36 -0500

    new pages and small corrections

--- maps/smoothresize.py
+++ maps/smoothresize.py
@@ -1,9 +1,11 @@
 import kaa.imlib2 as im
 import glob
 
+N = 512
+
 #for pic in glob.glob('/var/www/runat.me/tmp/maps/*/*/*.png'):
-for pic in glob.glob('/var/www/runat.me/tmp/maps/zoom/*.png'):
+for pic in glob.glob('/var/www/runat.me/tmp/maps/energyfar/*.png'):
     z = im.open(pic)
-    if z.size == (1024,1024):
-        b = z.scale((512,512))
+    if z.size == (N,N):
+        b = z.scale((N/2,N/2))
         b.save(pic)
