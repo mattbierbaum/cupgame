c0ebe8823de497b397b68da7636b088c96f77085
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-01-13 19:35:32 -0500

    refresh parameters to help cache

--- maps/smoothresize.py
+++ maps/smoothresize.py
@@ -1,8 +1,9 @@
 import kaa.imlib2 as im
 import glob
 
-for pic in glob.glob('/var/www/runat.me/tmp/maps/*/*/*.png'):
+#for pic in glob.glob('/var/www/runat.me/tmp/maps/*/*/*.png'):
+for pic in glob.glob('/var/www/runat.me/tmp/maps/zoom/*.png'):
     z = im.open(pic)
-    if z.size == (512,512):
-        b = z.scale((256,256))
+    if z.size == (1024,1024):
+        b = z.scale((512,512))
         b.save(pic)
